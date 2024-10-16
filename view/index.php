1ce03cd6212bbc64eeb672ec81a4daf2e7ee8a87
LeticiaFreitas79 <leticiasilvfreitas@gmai.com>
2024-10-16 14:33:40 -0300

    index.php; adicionando arquivos css e js

---
+++ view/index.php
@@ -0,0 +1,21 @@
+<!--Objetivo do Código: Página inicial do site onde deve ser exibido os destaques da Clínica Neko-->
+<!--Status do Código: Em desenvolvimento-->
+
+<!DOCTYPE html>
+<html lang="pt_br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!--CSS-->
+    <link href="../view.css/bootstrap.min.css" rel="stylesheet">
+    <title>Home | Clínica Neko</title>
+    
+</head>
+<body>
+    
+    <!--JavaScript-->
+    <script src="../view_js/bootstrap.bundle.min.js"></script>
+  </body>
+</body>
+</html>
