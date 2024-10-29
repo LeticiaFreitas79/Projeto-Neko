5807ae61bf0d4b68ccd0e54b00f14647efcf2d1b
LeticiaFreita79 <leticiasilvfreitas@gmail.com>
2024-10-29 18:09:03 -0300

    index.php - testando destaques

--- view/index.php
+++ view/index.php
@@ -38,12 +38,33 @@
     </div>
 
     <!--Veterinário-->
-      <div id="Card01" class="card" style="width: 18rem;">
-        <img src="../view_img/cachorro-estetoscopio.webp" class="card-img-top" alt="...">
-          <div class="card-body">
-            <p class="card-text">PRONTO SOCORRO 24H</p>
-          </div>
-      </div>
+    <div id="div-titulo">
+      <h2 id="titulo">VETERINÁRIO</h2>
+
+      <div class="container-destaqueVet">
+
+        <div id="CardVet" class="card">
+          <img src="../view_img/cachorro-estetoscopio.webp" class="card-img" alt="...">
+        </div>
+        
+        <div id="CardVet" class="card">
+          <img src="../view_img/cachorro-estetoscopio.webp" class="card-img" alt="...">
+        </div>
+        
+        <div id="CardVet" class="card">
+          <img src="../view_img/cachorro-estetoscopio.webp" class="card-img" alt="...">
+        </div>
+      </div> <!-- Fecha a 'container-destaqueVet'-->
+    
+      <button type="button" id="btnNav" class="btn">
+      <a href="principal_veterinario.php">Clique aqui para ver mais
+        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-right-circle-fill" viewBox="0 0 16 16">
+          <path d="M8 0a8 8 0 1 1 0 16A8 8 0 0 1 8 0M4.5 7.5a.5.5 0 0 0 0 1h5.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5z"/>
+        </svg>
+      </a>
+    </button>
+
+    </div> <!--Fecha 'div-titulo'-->
 
     <!--Loja-->
     <!--Servicos-->
