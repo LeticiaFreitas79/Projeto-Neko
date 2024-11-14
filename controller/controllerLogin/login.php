3b0c2ea0423639245ac4db2b48f5f1c70aab9cb0
Letícia Freitas <LeticiaSFreitas@gmail.com>
2024-11-13 23:18:15 -0300

    controllerLogin/login.php - testando comandos php version2

--- controller/controllerLogin/login.php
+++ controller/controllerLogin/login.php
@@ -14,40 +14,22 @@
 
     //VERIFICAR SE O FORMULÁRIO EM (view_login/index_login.php) FOI ENVIADO.
     //Método PHP usado para enviar o formulário.
-    if ($_SERVER['REQUEST_METHOD'] === 'POST')
+    if ($_POST)
     {
         //PUXAR OS DADOS INSERIDOS NO FORMULÁRIO DE LOGIN (view_login/index_login.php).
-        $emailUsuario = trim($_POST['emailUsuario']); //A função 'trim' remove espaços em branco foram inseridos no campo pelo usuário, evitando possíveis erros.
-        $senhaUsuario = $_POST['senhaUsuario'];
+        $emailUsuario = ($_POST['emailUsuario']);
+        $senhaUsuario = password_hash($_POST['senhaUsuario']); //'password_hard' serve para criptografar a senha. É uma medida de segurança.
     }
 
-    //VERIFICAR SE O USUÁRIO EXISTE NO BANCO DE DADOS.
+    //CONSULTAR O BANCO DE DADOS.
+    $emailConsulta = $conexao -> query ("SELECT * FROM usuario WHERE email = '$emailUsuario' and senha '$senhaUsuario'"); //Comando SQL que consulta se as informações existem no Banco de Dados.
+    $emailRow = $emailConsulta -> fetch_assoc(); //O resultado da consulta é armazenado na váriavel 'emailRow' a tornando em um array associativo através do 'fetch_assoc()'.
+    $numeroRow = $emailConsulta -> num_rows; //Armazena o número de linhas compatové com as linhas puxadas do Banco de Dados.
+
     //VERIFICAR A SENHA DO USUÁRIO ENCONTRADO.
     //LOGIN REALIZADO.
     //SENHA INCORRETA.
     //USUÁRIO INEXISTENTE.
     //ACESSO INVÁLIDO.
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    class Usuario
-    {
-        //Propriedades da Classe "Usuario".
-        public $usuario;
-        public $senha;
-
-
-    }
 ?>
