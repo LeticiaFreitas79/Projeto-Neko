a8b14716b76b0ece402852496602a1d67f688440
Letícia Freitas <LeticiaSFreitas@gmail.com>
2024-11-16 00:39:29 -0300

    controllerLogin/cadastroCliente.php - desenvolvendo

--- controller/controllerLogin/cadastroCliente.php
+++ controller/controllerLogin/cadastroCliente.php
@@ -1,2 +1,21 @@
 <!--Objetivo do Código: Puxa as informações em "login_cadastro.php" e as insere no Banco de Dados-->
 <!--Status do Código: Em desenvolvimento-->
+
+<?php
+    //Confirmando que o formulário foi enviado.
+    if (isset($_POST['submit']))
+    {
+       //REGISTRO DOS DADOS.
+        if (!isset($_SESSION));
+        session_start();
+
+        foreach ($_POST as $chave => $valor) $_SESSION[$chave] = $valor;
+
+       //VALIDAÇÃO DOS DADOS.
+       if ()
+
+       //INSERÇÃO NO BANCO DE DADOS.
+
+       //REDIRECIONAMENTO.
+    }
+?>
