e93cb373df1290dc33f8ead72574f445f9fd4b02
Letícia Freitas <LeticiaSFreitas@gmail.com>
2024-11-16 15:51:11 -0300

    controllerLogin/acessoCliente.php - corrigindo

--- controller/controllerLogin/acessoCliente.php
+++ controller/controllerLogin/acessoCliente.php
@@ -16,36 +16,3 @@
                 </p>"); //Revisar
     }
 ?>
-
-
-
-
-
-
-
-
-
-
-
-<?php
-//PARTE DA ISA; a substituir ou editar
-    session_name('clinicanekodb');
-    if (!isset($_SESSION)) {
-        session_start();
-    }
-    // segurança digital
-
-    // verificar se o usuário está logado na sessão
-    if(!isset($_SESSION['login_usuario'])){
-        // se não existir, redirecionamos a sessão por segurança
-        header('location: login.php'); // redirecioamento
-        exit;
-    } 
-    $nome_da_sessao = session_name();
-    if(!isset($_SESSION['nome_da_sessao']) 
-        or ($_SESSION['nome_da_sessao']!=$nome_da_sessao)){
-            session_destroy();
-            
-    }
-
-?>
